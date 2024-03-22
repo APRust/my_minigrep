2a8aaf71973b8d518cb00c00bafbe7b1599b7579
APRust <alex.pikalov.rust@gmail.com>
2024-03-22 18:59:08 +0300

    move code to lib.rs, start tdd

---
+++ my_minigrep/src/lib.rs
@@ -0,0 +1,38 @@
+use std::error::Error;
+use std::fs;
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(&config.file_path)?;
+    Ok(())
+}
+fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn one_search() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
--- my_minigrep/src/main.rs
+++ my_minigrep/src/main.rs
@@ -1,5 +1,7 @@
 use std::error::Error;
-use std::{env, fs, process};
+use std::{env, process};
+
+use my_minigrep::{run, Config};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
@@ -7,35 +9,10 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("Problem with parsing arguments! {}", e);
         process::exit(1);
     });
-    println!("Searching for... {}", &config.query);
-    println!("File path: {}\n", &config.file_path);
 
     if let Err(error) = run(config) {
         println!("Application error: {}", error);
         process::exit(1);
     };
     Ok(())
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(&config.file_path)?;
-
-    println!("Text contains: \n{contents}");
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
-    }
 }
